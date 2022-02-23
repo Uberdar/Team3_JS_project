c6948617e8321afc30db8a39d58bcef57b5ba0c4
Alexandr Sanatar <allexvik@gmail.com>
2022-02-23 16:10:45 +0200

    Update movieAPI.js

--- src/js/movieAPI.js
+++ src/js/movieAPI.js
@@ -7,7 +7,7 @@ export default class APIQuery {
     }
     async TrandingMovies(){
         try {
-            const projAPI = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${this.API_KEY}`);
+            const projAPI = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${this.API_KEY}`);
             const projAPI_JSONED = await projAPI.json();
             // console.log(projAPI_JSONED);
             return projAPI_JSONED;
