2177b34df6969830962eaf66c52972d6625d8e5e
Alexandr Sanatar <allexvik@gmail.com>
2022-02-24 03:00:57 +0200

    Update demo_test_js.js

--- src/js/demo_test_js.js
+++ src/js/demo_test_js.js
@@ -2,6 +2,26 @@ import DEMO from './movieAPI';
 import Pagination from 'tui-pagination'; /* ES6 */
 import markup from './makeMarkup';
 import {Spinner} from 'spin.js';
+var opts = {
+    lines: 13, // The number of lines to draw
+    length: 80, // The length of each line
+    width: 22, // The line thickness
+    radius: 70, // The radius of the inner circle
+    scale: 1.45, // Scales overall size of the spinner
+    corners: 1, // Corner roundness (0..1)
+    speed: 0.8, // Rounds per second
+    rotate: 0, // The rotation offset
+    animation: 'spinner-line-shrink', // The CSS animation name for the lines
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: 'black', // CSS color or array of colors
+    fadeColor: 'transparent', // CSS color or array of colors
+    top: '49%', // Top position relative to parent
+    left: '50%', // Left position relative to parent
+    shadow: '0 0 1px transparent', // Box-shadow for the lines
+    zIndex: 2000000000, // The z-index (defaults to 2e9)
+    className: 'spinner', // The CSS class to assign to the spinner
+    position: 'absolute', // Element positioning
+  };
 const ClassInstance = new DEMO(); //создаем екземпляр класса
 
 
