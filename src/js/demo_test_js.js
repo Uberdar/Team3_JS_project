f2c464d3c78261445c18356ddaa2d9122e4465a0
Alexandr Sanatar <allexvik@gmail.com>
2022-02-24 02:59:38 +0200

    v1

--- src/js/demo_test_js.js
+++ src/js/demo_test_js.js
@@ -4,53 +4,82 @@ import markup from './makeMarkup';
 import {Spinner} from 'spin.js';
 const ClassInstance = new DEMO(); //создаем екземпляр класса
 
-document.getElementById("poster").style.display="none";
+
 var debounce = require('debounce'); //npm install debounce
 
-let pagesVar = 1;
+// let pagesVar = 1;
 async function DEMO_TRANDING_MOVIES() {
 
   const demox = await ClassInstance.TrandingMovies(); //присваиваем промис TrandingMovies() константе
-  console.log('demox: ', demox); // смотрим что получилось
-  const demox_results = demox.results; // делаем выборку из обьекта который получился выше
-  console.log('demox_results: ', demox_results); // смотрим что получилось
+//   console.log('demox: ', demox); // смотрим что получилось
+//   const demox_results = demox.results; // делаем выборку из обьекта который получился выше
+//   console.log('demox_results: ', demox_results); // смотрим что получилось
+  const container = document.getElementById('pagination');
+  const options = { // below default value of options
+       totalItems: demox.total_results,
+       itemsPerPage: 20,
+       visiblePages: 5,
+       page: 1,
+       centerAlign: false,
+       firstItemClassName: 'tui-first-child',
+       lastItemClassName: 'tui-last-child',
+       usageStatistics:false,
+       template: {
+           page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+           currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+           moveButton:
+               '<a href="#" class="tui-page-btn tui-{{type}}">' +
+                   '<span class="tui-ico-{{type}}">{{type}}</span>' +
+               '</a>',
+           disabledMoveButton:
+               '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+                   '<span class="tui-ico-{{type}}">{{type}}</span>' +
+               '</span>',
+           moreButton:
+               '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+                   '<span class="tui-ico-ellip">...</span>' +
+               '</a>'
+       }
+  
+  };
+  
+  
+  const pagination = new Pagination(container, options);
+  
+//   pagination.on("afterMove", async function testfun(event){
+//       const { page } = event;
+//       ClassInstance.testpage = page;
+//       const testvar = await ClassInstance.GetParticularPage();
+//     //   console.log('testvar: ', testvar.results);
+//     //   console.log(page);
+//       // console.log(document.querySelector(".gallery"));
+//       document.querySelector(".gallery").innerHTML = '';
+//       document.querySelector(".gallery").innerHTML = markup(testvar.results);
+//   }
+//   );
+function test(event){
+    spinner.spin(target);
+    testfun(event);
 
+}
+async function testfun(event){
+    const { page } = event;
+    ClassInstance.testpage = page;
+    const testvar = await ClassInstance.GetParticularPage();
+  //   console.log('testvar: ', testvar.results);
+  //   console.log(page);
+    // console.log(document.querySelector(".gallery"));
+    document.querySelector(".gallery").innerHTML = '';
+    document.querySelector(".gallery").innerHTML = markup(testvar.results);
+
+}
+  pagination.on("afterMove", debounce(test,1500)
+
+  );
 //   demox_results.map(x => console.log('Trending_results:', x.original_name, x.title, x.id));
 }
 DEMO_TRANDING_MOVIES(); // инициируем функцию чтоб все выше описанное выполнилось
 
-const container = document.getElementById('pagination');
-
-const options = { // below default value of options
-     totalItems: 200,
-     itemsPerPage: 20,
-     visiblePages: 5,
-     page: 1,
-     centerAlign: false,
-     firstItemClassName: 'tui-first-child',
-     lastItemClassName: 'tui-last-child',
-     usageStatistics:false,
-     template: {
-         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-         moveButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</a>',
-         disabledMoveButton:
-             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</span>',
-         moreButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                 '<span class="tui-ico-ellip">...</span>' +
-             '</a>'
-     }
-
-};
-
-
-const pagination = new Pagination(container, options);
 
 
 // console.log('container: ', container.childNodes);
@@ -71,37 +100,24 @@ const pagination = new Pagination(container, options);
 //     console.log(page);
 //   });
 
-pagination.on("afterMove", async function testfun(event){
-    const { page } = event;
-    ClassInstance.testpage = page;
-    const testvar = await ClassInstance.GetParticularPage();
-    console.log('testvar: ', testvar.results);
-    console.log(page);
-    // console.log(document.querySelector(".gallery"));
-    document.querySelector(".gallery").innerHTML = '';
-    document.querySelector(".gallery").innerHTML = markup(testvar.results);
 
-}
-
-);
 
 const x = debounce(() => {
-    document.getElementById("poster").style.display="block";
-    return DETAILED_INFO_MOVIES();
+
+    // document.getElementById("poster").style.display="block";
+    // return DETAILED_INFO_MOVIES();
 }, 1500);
-x();
 
 
-
-var target = document.getElementById('testspinner');
-var spinner = new Spinner(opts).stop(target);
+debounce(test(), 500)
+// var target = document.getElementById('testspinner');
+// var spinner = new Spinner(opts).stop(target);
 
 function test(){
     spinner.spin(target);
     x();
 
 }
-test();
 
 
 // async function DEMO_SEARCH_MOVIES() {
